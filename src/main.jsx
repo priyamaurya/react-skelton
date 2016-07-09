466220004007ab9a0b648371a6cc2a399fdf1b7c
haripillai <hari.try@gmail.com>
2016-07-09 16:31:25 +0100

    react-skelton

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDom = require('react-dom'); 
+
+var ListA = require('./components/List.jsx');
+
+
+ReactDom.render(<ListA/>,document.getElementById('ingredients'));
